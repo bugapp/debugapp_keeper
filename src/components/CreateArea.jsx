962d3044bc4114c93d35784b1d54c76aef733a63
Felipe Sousa <felipe.sousa4030@gmail.com.br>
2022-05-25 21:23:27 -0300

    feat: Add CreateArea Component

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from "@material-ui/icons/Add";
-import { Fab } from "@material-ui/core";
-// import { Zoom } from "@material-ui/core";
+import { Fab, Zoom } from "@material-ui/core";
 
 const CreateArea = (props) => {
+  const [focus, isFocus] = useState(false);
+
+  const handleClick = (event) => {
+    event.target.style.borderBottom = "none";
+    event.target.style.borderRadius = "0.5em 0.5em 0em 0em";
+    isFocus(true);
+  };
+
   return (
     <form className="form">
       <input
         type="text"
         className="form__title"
         placeholder="Title"
+        onClick={handleClick}
       />
-      <textarea rows="3" className="form__content" placeholder="Content"></textarea>
-      <Fab className="form__button" size="small">
-        <AddIcon />
-      </Fab>
+      {focus && (
+        <textarea
+          rows="3"
+          className="form__content"
+          placeholder="Content"
+          resize="none"
+        ></textarea>
+      )}
+      <Zoom in={focus}>
+        <Fab className="form__button" disabled={!focus}>
+          <AddIcon className="button__icon" />
+        </Fab>
+      </Zoom>
     </form>
   );
 };

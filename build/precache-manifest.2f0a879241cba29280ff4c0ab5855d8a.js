29a024845b55024fa955b1f61995d320d8d8fd2b
Felipe Sousa <felipe.sousa4030@gmail.com.br>
2022-05-27 20:33:25 -0300

    feat: Add build directory

---
+++ build/precache-manifest.2f0a879241cba29280ff4c0ab5855d8a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88682838c08eba4d76657c91626a0790",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e455e228ae3ef7d8146",
+    "url": "/static/js/2.d6fcef00.chunk.js"
+  },
+  {
+    "revision": "dd6b3107dc7042989a7e",
+    "url": "/static/js/main.13047409.chunk.js"
+  },
+  {
+    "revision": "63408c8ea8b895fcc299",
+    "url": "/static/js/runtime-main.fab1aa44.js"
+  }
+]);
